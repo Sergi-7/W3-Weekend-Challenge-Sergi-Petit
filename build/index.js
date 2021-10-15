49436f1b87ad599d338a8f4d3a43e7a010fba105
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-16 00:01:05 +0200

    Added Page & Page.test

--- build/index.js
+++ build/index.js
@@ -1,3 +1,6 @@
 import Component from "./js/Component.js";
+import Page from "./js/Page.js";
 
 const mainDiv = new Component(document.body, "main-div", "div");
+const bigDiv = document.querySelector(".main-div");
+const test = new Page(bigDiv, "test", "span");
--- build/js/Page.test.js
+++ build/js/Page.test.js
@@ -1,10 +1,10 @@
-import Component from "./Component.js";
+import Page from "./Page.js";
 
-describe("Given a Component class", () => {
+describe("Given a Page class", () => {
   describe("when it receives a div container, a test class y a span tag", () => {
     test("Then it should render a span element with a test class inside the div container", () => {
       const container = document.createElement("div");
-      const p = new Component(container, "text", "span");
+      const newSpan = new Page(container, "text", "span");
       const span = document.querySelector(".text");
       expect(span).not.toBeNull();
     });
