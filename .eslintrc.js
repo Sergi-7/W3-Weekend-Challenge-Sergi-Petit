b2074c320af54a5376e4d26d508ed85f977ce247
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-15 18:03:14 +0200

    Add initial files & eslint, jest , sass instalation

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "class-method-use-this": "off",
+    "lines-between-class-members": "off",
+    "import/extensions": "off",
+  },
+};
