f34ccfe0ee7fb8b5253e213d699e633a39c6d75c
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-16 00:18:20 +0200

    Added tests for Component & Page , both OK

--- build/js/Page.test.js
+++ build/js/Page.test.js
@@ -1,12 +1,19 @@
 import Page from "./Page.js";
 
-describe("Given a Page class", () => {
-  describe("when it receives a div container, a test class y a span tag", () => {
-    test("Then it should render a span element with a test class inside the div container", () => {
+describe("Given a component class", () => {
+  describe("When it receives a div container, a 'text' class and a 'span' tag", () => {
+    test("Then it should render a span element with a 'text' class inside the div container", () => {
+      // Arrange
       const container = document.createElement("div");
-      const newSpan = new Page(container, "text", "span");
-      const span = document.querySelector(".text");
-      expect(span).not.toBeNull();
+      const className = "text";
+      const tag = "span";
+
+      // Act
+      const result = new Page(container, className, tag);
+      const newSpan = container.querySelector("span.text");
+
+      // Assert
+      expect(newSpan).not.toBeNull();
     });
   });
 });
