accbff8953c92cb1fa1c2b517ca50b44010abfa8
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-16 18:20:27 +0200

    Add PokemonService class

--- build/index.js
+++ build/index.js
@@ -1,6 +1,7 @@
 import Component from "./js/Component.js";
 import Page from "./js/Page.js";
 import Card from "./js/Card.js";
+import PokemonService from "./PokemonService.js";
 
 const mainDiv = new Component(document.body, "main-div", "div");
 const bigDiv = document.querySelector(".main-div");
@@ -9,3 +10,14 @@ const test = new Page(bigDiv, "header-nav", "nav");
 const newCard = new Card(bigDiv, "pokemon-list");
 
 const testElement = document.querySelector(".test");
+
+const pokemonApi = new PokemonService();
+
+const getPokemonArray = (async () => {
+  const pokemonArray = await pokemonApi.getPokemonData();
+  const pokemonData = await pokemonArray.results;
+  console.log(pokemonData);
+  return pokemonData;
+})();
+
+console.log(getPokemonArray[0]);
